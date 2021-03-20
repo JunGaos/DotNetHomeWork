516334fc204220feee8dffb02dce22731acc6d13
Simon-max-pixel <2641945063@qq.com>
2021-03-20 22:19:20 +0800

    第四次作业

---
+++ Homework4/AlarmClock/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlarmClock
+{
+    public delegate void TickHanlder(object sender, DateTime args);
+    public delegate void AlarmHandler(object sender, DateTime args);
+    public class Clock
+    {
+        public event TickHanlder Tick;
+        public event AlarmHandler Alarm;
+
+        private DateTime time;
+        private DateTime alarmtime;
+        public DateTime Time { get => time; set => time = value; }
+        public DateTime AlarmTime { get => alarmtime; set => alarmtime = value; }
+
+        public Clock()
+        {
+            time = DateTime.Now;
+        }
+        public void Begin()
+        {
+            while (true)
+            {
+                DateTime nowTime = DateTime.Now;
+                Tick(this, nowTime);
+                if (nowTime.ToString() == alarmtime.ToString())
+                {
+                    Alarm(this, alarmtime);
+                }
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Clock c = new Clock();
+            c.Tick += ShowTick;
+            c.Alarm += ShowAlarm;
+            c.AlarmTime = c.Time.AddSeconds(10);
+            c.Begin();
+        }
+        static void ShowTick(object sender, DateTime time)
+        {
+            Console.WriteLine("当前时间:" + time);
+        }
+        static void ShowAlarm(object sender, DateTime time)
+        {
+            Console.WriteLine("闹钟时间:" + time);
+        }
+    }
+}
---
+++ Homework4/ForEach/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { set; get; }
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+        public Node<T> Head
+        {
+            get => head;
+        }
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            for (Node<T> m = head; m.Next != null; m = m.Next)
+            {
+                action(m.Data);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> l = new GenericList<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                l.Add(i);
+            }
+            int sum = 0;
+            int max = l.Head.Data;
+            int min = l.Head.Data;
+
+            l.ForEach(x => Console.WriteLine(x));
+            l.ForEach(x => sum += x);
+            l.ForEach(x => { max = max > x ? max : x; });
+            l.ForEach(x => { min = min < x ? min : x; });
+
+            Console.WriteLine("最大值是：" + max);
+            Console.WriteLine("最小值是：" + min);
+            Console.WriteLine("总和是：" + sum);
+            Console.ReadKey();
+        }
+    }
+}
